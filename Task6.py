30eb86f7c8838866629973513e97b64dfaac256f
oksanasuldina <oksanasuldina@yandex.ru>
2021-07-18 21:53:55 +0300

    Update Task6.py

--- Task6.py
+++ Task6.py
@@ -2,6 +2,9 @@ dict_lessons = {}
 with open("lessons.txt", encoding="utf-8") as lessons_file:
     for line in lessons_file:
         name, hours = line.split(':')
-        name_sum = sum(map(int, "".join([i for i in hours if i == ' ' or ('0' <= i <= '9')]).split()))
-        dict_lessons[name] = name_sum
+        str_hours = ''
+        for el in hours:
+            if el == ' ' or ('0' <= el <= '9'):
+                str_hours += el
+        dict_lessons[name] = sum(map(int, str_hours.split()))
 print(f"{dict_lessons}")
